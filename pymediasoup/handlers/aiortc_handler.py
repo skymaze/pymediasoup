16c5e76cf1d2ce8bf17aa0bd41c2417c6edeefd4
Jiang Yue <jiangyue@aixn.io>
2020-10-23 13:30:24 +0800

    Add aiortc handler run()

--- pymediasoup/handlers/aiortc_handler.py
+++ pymediasoup/handlers/aiortc_handler.py
@@ -6,6 +6,7 @@ from .sdp.remote_sdp import RemoteSdp
 from .sdp import common_utils
 from .handler_interface import HandlerInterface
 from ..rtp_parameters import RtpParameters
+from ..ortc import getSendingRtpParameters, getSendingRemoteRtpParameters
 
 
 SCTP_NUM_STREAMS = { 'OS': 1024, 'MIS': 1024 }
@@ -64,3 +65,65 @@ class AiortcHandler(HandlerInterface):
         return {
             'numStreams': SCTP_NUM_STREAMS
         }
+    
+    def run(
+        self,
+        direction,
+        iceParameters,
+        iceCandidates,
+        dtlsParameters,
+        sctpParameters,
+        iceServers,
+        iceTransportPolicy,
+        additionalSettings,
+        proprietaryConstraints,
+        extendedRtpCapabilities
+    ):
+        logging.debug('run()')
+        self._direction = direction
+        self._remoteSdp = RemoteSdp(
+            iceParameters=iceParameters,
+            iceCandidates=iceCandidates,
+            dtlsParameters=dtlsParameters,
+            sctpParameters=sctpParameters
+        )
+        self._sendingRtpParametersByKind = {
+            'audio': getSendingRtpParameters('audio', extendedRtpCapabilities),
+            'video': getSendingRtpParameters('video', extendedRtpCapabilities)
+        }
+        self._sendingRemoteRtpParametersByKind = {
+            'audio': getSendingRemoteRtpParameters('audio', extendedRtpCapabilities),
+            'video': getSendingRemoteRtpParameters('video', extendedRtpCapabilities)
+        }
+        self._pc = RTCPeerConnection()
+        @self._pc.on('iceconnectionstatechange')
+        def on_iceconnectionstatechange():
+            if self._pc.iceConnectionState == 'checking':
+                self.emit('@connectionstatechange', 'connecting')
+            elif self._pc.iceConnectionState in ['connected', 'completed']:
+                self.emit('@connectionstatechange', 'connected')
+            elif self._pc.iceConnectionState == 'failed':
+                self.emit('@connectionstatechange', 'failed')
+            elif self._pc.iceConnectionState == 'disconnected':
+                self.emit('@connectionstatechange', 'disconnected')
+            elif self._pc.iceConnectionState == 'closed':
+                self.emit('@connectionstatechange', 'closed')
+        
+    async def updateIceServers(self, iceServers):
+        logging.debug('updateIceServers() passed')
+        # TODO: aiortc can not update iceServers
+    
+    async def restartIce(self, iceParameters):
+        logging.debug('restartIce()')
+        self._remoteSdp.updateIceParameters(iceParameters)
+        if not self._transportReady:
+            return
+        if self._direction == 'send':
+            # NOTE: aiortc RTCPeerConnection createOffer do not have iceRestart
+            offer = await self._pc.createOffer()
+            logging.debug(f'restartIce() | calling pc.setLocalDescription() [offer:{offer}]')
+            await self._pc.setLocalDescription(offer)
+            answer = {
+                'type': 'answer',
+                'sdp': self._remoteSdp.getSdp()
+            }
